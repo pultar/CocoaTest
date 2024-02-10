77d8163cd7e82ee8a4b39b5413658d845144d82d
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2024-02-10 21:53:33 +0100

    Initial Commit

---
+++ CocoaTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CocoaTest
+//
+//  Created by Felix Pultar on 10.02.2024.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CocoaTest/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  CocoaTest
+//
+//  Created by Felix Pultar on 10.02.2024.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
